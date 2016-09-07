a495061329e0c433e7c0bfb11aa2dd4a7467fa9b
Duoc Nguyen <nguyenvanduocit@gmail.com>
2016-09-07 09:26:23 +0700

    remove sample

--- src/vuex/setting/actions.js
+++ src/vuex/setting/actions.js
@@ -1,8 +1,6 @@
 import httpService from './../../services/http'
 export const loadSettings = ({dispatch, state}) => {
   httpService.getSettings().then((response) => {
-    dispatch('SETTING.TITLE.SET', 'Site title')
-    dispatch('SETTING.TAGLINE.SET', 'Site tagline')
     let settings = response.json()
     dispatch('SETTING.TITLE.SET', settings.title)
     dispatch('SETTING.TAGLINE.SET', settings.tagline)
