235f8e701d23ad18eea6bfad68400aa399eed021
Duoc Nguyen <nguyenvanduocit@gmail.com>
2016-09-07 09:38:55 +0700

    add footer component

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
       <a class="skip-link screen-reader-text" href="#content">Skip to content</a>
       <sidebar></sidebar>
       <!-- main -->
+      <site-footer></site-footer>
     </div>
   </div>
 </template>
@@ -11,12 +12,14 @@
 <script>
 import store from './vuex/store'
 import Sidebar from './components/Sidebar.vue'
+import SiteFooter from './components/Footer.vue'
 import {loadSettings} from './vuex/setting/actions'
 export default {
   replace: false,
   store,
   components: {
-    Sidebar
+    Sidebar,
+    SiteFooter
   },
   vuex: {
     actions: {
---
+++ src/components/Footer.vue
@@ -0,0 +1,7 @@
+<template>
+    <footer id="colophon" class="site-footer" role="contentinfo">
+        <div class="site-info">
+            <a href="https://wordpress.org/">Proudly powered by WordPress</a>
+        </div><!-- .site-info -->
+    </footer>
+</template>
