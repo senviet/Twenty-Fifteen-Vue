4c3d7a1bbc67a3c483720c5589c6d086d700adc4
Tat Thien <tatthien.hcmus@gmail.com>
2016-09-08 17:00:26 +0700

    add icon font: Genericons

--- src/components/Sidebar/Menus/MainMenuItem.vue
+++ src/components/Sidebar/Menus/MainMenuItem.vue
@@ -1,14 +1,28 @@
 <template>
   <li id="menu-item-{{ item.id }}" class="menu-item-{{ item.id }}">
     <a href="{{ item.url }}">{{ item.title }}
-      <div class="menu-item-description">{{ item.description }}</div>
+      <div class="menu-item-description" v-if="item.description">{{ item.description }}</div>
     </a>
+
+    <template v-if="hasSubMenu">
+      <button class="dropdown-toggle" aria-expanded="false">
+        <span class="screen-reader-text">collapse child menu</span>
+      </button>
+      <ul class="submenu">
+        <!-- TODO can render cho nay -->
+      </ul>
+    </template>
   </li>
 </template>
 
 <script>
 export default{
-  props: ['item']
+  props: ['item'],
+  computed: {
+    hasSubMenu () {
+      return this.item.children
+    }
+  }
 }
 </script>
 
