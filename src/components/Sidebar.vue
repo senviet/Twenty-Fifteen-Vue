5af93198af2cae3a49a024def11d07a6610cfa36
Tat Thien <tatthien.hcmus@gmail.com>
2016-09-12 15:30:48 +0700

    add toggle for sidebar on responsive

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="sidebar" class="sidebar">
         <header-widget></header-widget>
-        <div id="secondary" class="secondary">
+        <div id="secondary" class="secondary" :class="{'toggled-on': toggleOn}">
           <menu-widget></menu-widget>
         </div>
     </div>
@@ -11,9 +11,20 @@
   import HeaderWidget from './Sidebar/Header.vue'
   import MenuWidget from './Sidebar/Menu.vue'
   export default{
+    data () {
+      return {
+        toggleOn: false
+      }
+    },
     components: {
       HeaderWidget,
       MenuWidget
+    },
+    events: {
+      'toggle-sidebar': function (toggleOn) {
+        this.toggleOn = toggleOn
+        console.log(this.toggleOn)
+      }
     }
   }
 </script>
--- src/components/Sidebar/Header.vue
+++ src/components/Sidebar/Header.vue
@@ -3,17 +3,28 @@
     <div class="site-branding">
       <h1 class="site-title"><a href="/" rel="home" v-text="title"></a></h1>
       <p class="site-description" v-text="tagline"></p>
-      <button class="secondary-toggle">Menu and widgets</button>
+      <button class="secondary-toggle" @click="toggle" :class="{'toggled-on': toggleOn}">Menu and widgets</button>
     </div><!-- .site-branding -->
   </header><!-- .site-header -->
 </template>
 <script>
   import {title, tagline} from './../../vuex/setting/getters'
   export default{
+    data () {
+      return {
+        toggleOn: false
+      }
+    },
     vuex: {
       getters: {
         title: title,
         tagline: tagline
+      }
+    },
+    methods: {
+      toggle () {
+        this.toggleOn = !this.toggleOn
+        this.$dispatch('toggle-sidebar', this.toggleOn)
       }
     }
   }
